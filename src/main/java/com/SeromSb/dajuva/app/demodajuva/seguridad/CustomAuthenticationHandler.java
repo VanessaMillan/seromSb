107c755474fc2a75e87fa4515ab9ca1f17aa4d68
VanessaMillan <millanvanessa18@gmail.com>
2023-04-10 17:30:10 -0500

    Agregar catch autenticacion

--- src/main/java/com/SeromSb/dajuva/app/demodajuva/seguridad/CustomAuthenticationHandler.java
+++ src/main/java/com/SeromSb/dajuva/app/demodajuva/seguridad/CustomAuthenticationHandler.java
@@ -3,6 +3,9 @@ package com.SeromSb.dajuva.app.demodajuva.seguridad;
 import java.io.IOException;
 import java.util.Collection;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.ExceptionTelemetry;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +17,24 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 
 @Configuration
 public class CustomAuthenticationHandler extends SimpleUrlAuthenticationSuccessHandler {
-
+	private static final TelemetryClient telemetryClient = new TelemetryClient();
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-
 		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-
-		if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("Administrador"))) {
-			setDefaultTargetUrl("/app/demodajuva/administrador/pagina-principal");
-		} else if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("Vendedor"))) {
-			setDefaultTargetUrl("/app/demodajuva/vendedor");
-		} else {
-			setDefaultTargetUrl("/cliente");
-		}
+		try {
+			if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("Administrador"))) {
+				setDefaultTargetUrl("/app/demodajuva/administrador/pagina-principal");
+			} else if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("Vendedor"))) {
+				setDefaultTargetUrl("/app/demodajuva/vendedor");
+			} else {
+				setDefaultTargetUrl("/cliente");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			ExceptionTelemetry telemetry = new ExceptionTelemetry(e);
+            telemetryClient.trackException(telemetry);
+		}	
 
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
